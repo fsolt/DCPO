0ff124cb105ba003e5f0be2d6d2efd06556d349f
Frederick Solt <fsolt@mac.com>
2015-11-05 15:32:24 -0600

    work towards making a function

--- R/dcpo.R
+++ R/dcpo.R
@@ -17,15 +17,31 @@ library(rstan)
 
 data1 <- gm
 
-dcpo_data <- list(  K=max(data1$ccode),
-                    T=max(data1$tcode),
-                    R=max(data1$rcode),
-                    N=length(data1$y_r),
-                    kk=data1$ccode,
-                    rr=data1$rcode,
-                    tt=data1$tcode,
-                    y_r=data1$y_r,
-                    n_r=data1$n
+dcpo <- function(x,
+                 model_code = NULL,
+                 seed = 324,
+                 iter = 100,
+                 cores = 1,
+                 chains = 4)
+
+### Delete these when turning into a function
+model_code <- dcpo_code
+seed <- 324
+iter <- 150
+cores <- 4
+chains <- 4
+x <- gm
+###
+
+dcpo_data <- list(  K=max(x$ccode),
+                    T=max(x$tcode),
+                    R=max(x$rcode),
+                    N=length(x$y_r),
+                    kk=x$ccode,
+                    rr=x$rcode,
+                    tt=x$tcode,
+                    y_r=x$y_r,
+                    n_r=x$n
 )
 
 dcpo_code <- '
@@ -102,64 +118,13 @@ dcpo_code <- '
     }
 '
 
-out1 <- stan(model_code = dcpo_code, data = dcpo_data, seed = 324, iter = 100, chains = 4)
+out1 <- stan(model_code = dcpo_code,
+             data = dcpo_data,
+             seed = seed,
+             iter = iter,
+             cores = cores,
+             chains = chains)
 
-x1 <- summary(out1)
-write.table(as.data.frame(x1$summary), file="x1.csv", sep = ",")
-View(as.data.frame(x1$summary))
-
-x2 <- summary(out1, pars="alpha", probs=c(.1, .9))
-x2 <- as.data.frame(x2$summary)
-
-x2$ccode <- as.numeric(gsub("alpha\\[([0-9]*),[0-9]*\\]", "\\1", row.names(x2)))
-x2$tcode <- as.numeric(gsub("alpha\\[[0-9]*,([0-9]*)\\]", "\\1", row.names(x2)))
-
-k <- data1 %>% group_by(country) %>% summarize(
-  ccode = first(ccode),
-  firstyr = first(firstyr),
-  lastyr = first(lastyr))
-
-#######HERE
-x2 <- merge(x2, k, all=T)
-x2$year <- min(x2$firstyr) + x2$tcode - 1
-
-x2 <- x2[which(x2$year <= x2$lastyr & x2$year >= x2$firstyr), c("country", "ccode", "year", "mean", "sd", "10%", "90%")]
-
-# Graph
-library(ggplot2)
-x2$kk <- x2$ccode
-
-pages <- c("1:35", "36:70", "71:105")
-
-x3 <- x2
-names(x3)[which(names(x3)=="10%")] <- "lb"
-names(x3)[which(names(x3)=="90%")] <- "ub"
-change <- ddply(x3, .(country), summarize,
-                ch1 = max(mean) - min(mean),
-                ch2 = mean[length(year)]- mean[1],
-                years = length(year)
-)
-change <- change[order(-change$ch1), ]
-
-plotlist <- list()
-for (i in 1:3) {
-    cpage <- unique(x3$country)[c(eval(parse(text=pages[i])))]
-    x4 <- x3[x3$country %in% cpage, ]
-    x4$country <- factor(x4$country, levels = cpage)
-
-    plotx <- ggplot(data=x4, aes(x=year, y=mean)) +
-        geom_line() +
-        theme(legend.position="none") +
-        coord_cartesian(xlim=c(1980,2013), ylim = c(0, 1)) +
-        labs(x = "Year", y = "Favorable Opinion") +
-        geom_ribbon(aes(ymin = lb, ymax = ub, linetype=NA), alpha = .25) +
-        facet_wrap(~country, ncol = 5) +
-        theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-    pdf(file=paste0("plot",i,".pdf"), width=6, height = 9)
-    plot(plotx)
-    graphics.off()
-}
 
 #Chime
 beep()

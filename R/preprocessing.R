8f77e99fe4de8c3fd455a2ab36745254dafcb090
Frederick Solt <fsolt@mac.com>
2015-11-28 10:36:18 -0600

    initial commit

---
+++ R/preprocessing.R
@@ -0,0 +1,16 @@
+library(readr)
+library(dplyr)
+
+library(foreign)
+library(haven)
+library(reshape2)
+library(beepr)
+library(rstan)
+
+datasets_table <- read_csv("data-raw/datasets_table.csv")
+gm0 <- read.csv("data-raw/gay_marriage_surveys.csv", as.is = TRUE)
+gm <- dcpo_setup(gm0)
+write_csv(gm, "data/all_data_gm.csv")
+
+gm <- read_csv("data/all_data_gm.csv")
+#gm1 <- gm %>% filter(firstyr!=lastyr)

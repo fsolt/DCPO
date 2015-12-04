6cb03a5649492a30ac89a9899ce4415a33fb1099
Frederick Solt <fsolt@mac.com>
2015-12-04 16:03:30 -0600

    add to package list

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -6,9 +6,9 @@ library(haven)
 library(reshape2)
 library(beepr)
 library(rstan)
+library(stringr)
 
 library(ggplot2)
-library(dotwhisker)
 
 datasets_table <- read_csv("data-raw/datasets_table.csv")
 gm0 <- read.csv("data-raw/gay_marriage_surveys.csv", as.is = TRUE)
@@ -16,4 +16,3 @@ gm <- dcpo_setup(gm0)
 write_csv(gm, "data/all_data_gm.csv")
 
 gm <- read_csv("data/all_data_gm.csv")
-#gm1 <- gm %>% filter(firstyr!=lastyr)

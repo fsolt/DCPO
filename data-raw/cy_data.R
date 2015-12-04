8e5b77cd37e8dd5fee79188a4cd970e77736b4de
Frederick Solt <fsolt@mac.com>
2015-12-03 19:15:30 -0600

    minor revisions

--- data-raw/cy_data.R
+++ data-raw/cy_data.R
@@ -1,3 +1,5 @@
+# functions and code toward generating the cy_data column of datasets_table.csv
+
 capwords <- function(s) {
     cap <- function(s) paste(toupper(substring(s, 1, 1)),
     {s <- substring(s, 2); tolower(s)}, sep = "", collapse = " " )
@@ -10,8 +12,8 @@ get.cy <- function(x, c_var = "country") {
     x$c_dcpo <- gsub("And ", "and ", x$c_dcpo)
     x$c_dcpo <- gsub("n Arab Jamahiriya", "", x$c_dcpo)
     x$c_dcpo <- gsub("n Federation", "", x$c_dcpo)
-    
-    cy_data <- ddply(x, .(get(id)), summarize, c_dcpo = c_dcpo[1], y_dcpo = round(mean(year)))
+
+    cy_data <- plyr::ddply(x, plyr::.(get(id)), summarize, c_dcpo = c_dcpo[1], y_dcpo = round(mean(year)))
     names(cy_data)[1] <- id
     dump("cy_data")
     temp <- readLines("dumpdata.R")
@@ -27,7 +29,7 @@ get.cy.no.id <- function(x, c_var = "country", orig_cvar, orig_yvar) {
     x$c_dcpo <- gsub("And ", "and ", x$c_dcpo)
     x$c_dcpo <- gsub("n Arab Jamahiriya", "", x$c_dcpo)
     x$c_dcpo <- gsub("n Federation", "", x$c_dcpo)
-    
+
     cy_data <- ddply(x, .(get(orig_cvar), get(orig_yvar)), summarize, c_dcpo = c_dcpo[1], y_dcpo = round(mean(year)))
     names(cy_data)[1] <- orig_cvar
     names(cy_data)[2] <- orig_yvar
@@ -42,7 +44,7 @@ get.cy.no.id <- function(x, c_var = "country", orig_cvar, orig_yvar) {
 encode <- function(x) {
     df <- strsplit(x, "\\$")[[1]][1]
     old.var <- strsplit(x, "\\$")[[1]][2]
-    dict <- data.frame(new.var = names(attributes(get(df))$label.table[[attributes(get(df))$val.labels[which(names(get(df))==old.var)]]]), 
+    dict <- data.frame(new.var = names(attributes(get(df))$label.table[[attributes(get(df))$val.labels[which(names(get(df))==old.var)]]]),
                        old.var = attributes(get(df))$label.table[[attributes(get(df))$val.labels[which(names(get(df))==old.var)]]])
     matches <- match(get(df)[, old.var], dict[, "old.var"])
     y <- as.character(dict[matches, "new.var"])
@@ -52,7 +54,7 @@ encode <- function(x) {
 library(countrycode)
 library(plyr)
 
-
+datapath <- "~/Documents/Projects/Data/"
 
 # ISSP
 issp.files <- c("Religion/Religion I/ZA2150.dta", "Family/Family II/ZA2620.dta", "Religion/Religion II/ZA3190.dta", "Religion/Religion III/ZA4950_v2-2-0.dta")
@@ -80,21 +82,21 @@ for (i in seq(length(issp.files))) {
         issp <- merge(issp, cc, all=T)
         issp$country <- countrycode(issp$cty, "country.name", "country.name")
         issp$country[issp$v3==5] <- "NORTHERN IRELAND"
-        id <- "v3"     
+        id <- "v3"
     } else if (grepl("ZA4950_v2-2-0.dta", issp.files[i])) {
-        issp$country <- countrycode(issp$V5, "iso3n", "country.name") 
+        issp$country <- countrycode(issp$V5, "iso3n", "country.name")
         issp$country[issp$V4==8262] <- "NORTHERN IRELAND"
         cc <- data.frame(country = countrycode(c("Australia", "Austria", "Belgium", "Chile", "Croatia", "Czech Republic", "Denmark", "Dominican Republic", "Finland", "France", "Germany", "Great Britain", "Hungary", "Ireland", "Israel", "Italy", "Japan", "Latvia", "Mexico", "Netherlands", "New Zealand", "NIplace", "Norway", "Philippines", "Poland", "Portugal", "Russia", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sweden", "Switzerland", "Taiwan", "Turkey", "Ukraine", "Uruguay", "United States", "Venezuela"), "country.name", "country.name"),
                          year = c(2009, 2008, 2008, 2008, 2009, 2008, 2009, 2008, 2008, 2008, 2008, 2008, 2008, 2008, 2009, 2008, 2008, 2009, 2009, 2008, 2008, 2008, 2008, 2007, 2010, 2009, 2009, 2008, 2009, 2008, 2008, 2008, 2008, 2008, 2009, 2008, 2008, 2008, 2008, 2008),
                          stringsAsFactors=F)
         cc$country[is.na(cc$country)] <- "NORTHERN IRELAND"
-        issp <- merge(issp, cc, all=T) 
+        issp <- merge(issp, cc, all=T)
         id <- "V4"
     }
     cy <- get.cy(issp)
     write(cy, file = paste0("issp", i, ".txt"))
 }
-    
+
 
 #EB774-12
 eb.files <- c("EB77_4-12/ZA5613_v2-0-0.dta")
@@ -153,6 +155,19 @@ surv <- tolower(gsub("[/-].*", "", eb.files))
 cy <- get.cy(eb)
 write(cy, file = paste0("cy_data/", surv, ".txt"))
 
+# CDCEE
+cee <- haven::read_dta("~/Documents/Projects/Data/CDCEE/ZA4054_F1.dta")
+cdcee <- haven::read_dta("~/Documents/Projects/Data/CDCEE/ZA4054_F1.dta"); cdcee <- dplyr::left_join(cdcee, data.frame(v666 = c(1:25, NA), y_dcpo = c(rep(1990, 3), rep(1991,3), 1992, rep(1998, 7), rep(1999, 5), rep(2000, 2), rep(2001, 4), 1990)))
+cee_year <- data.frame(v666 = c(1:25, NA), year = c(rep(1990, 3), rep(1991,3), 1992, rep(1998, 7), rep(1999, 5), rep(2000, 2), rep(2001, 4), 1990))
+cee_country <- data.frame(v3 = 1:15, c_dcpo = c("Belarus", "Bulgaria", "Czech Republic", "Estonia", "Germany", "Germany", "Hungary", "Latvia", "Lithuania", "Poland", "Romania", "Russia", "Slovakia", "Slovenia", "Ukraine"))
+cee1 <- left_join(cee, cee_year)
+cee2 <- left_join(cee1, cee_country)
+t_data <- left_join(cdcee, cee_country)
+wt <- t_data$v633; wt[!is.na(t_data$v634)] <- t_data$v634[!is.na(t_data$v634)]; wt[!is.na(t_data$v636)] <- t_data$v636[!is.na(t_data$v636)]; wt[!is.na(t_data$v637)] <- t_data$v637[!is.na(t_data$v637)]; wt[!is.na(t_data$v638)] <- t_data$v638[!is.na(t_data$v638)]; wt[!is.na(t_data$v639)] <- t_data$v639[!is.na(t_data$v639)]; wt[!is.na(t_data$v640)] <- t_data$v640[!is.na(t_data$v640)]; wt[is.na(wt)] <- 1
+
+wt <- t_data$v8; wt[grep("DE\\-", t_data$isocntry)] <- t_data$v12[grep("DE\\-", t_data$isocntry)]; wt[grep("GB\\-", t_data$isocntry)] <- t_data$v10[grep("GB\\-", t_data$isocntry)]
+
+data.frame(S025 = c(81998, 82002, 122002, 122013, 202005, 311997, 312011, 321984, 321991, 321995, 321999, 322006, 361981, 361995, 362005, 362012, 501996, 502002, 511997, 512011, 702001, 761991, 762006, 1001997, 1002005, 1121990, 1121996, 1122011, 1242000, 1242006, 1521990, 1521996, 1522000, 1522006, 1522011, 1561990, 1561995, 1562001, 1562007, 1562012, 1582006, 1582012, 1701997, 1701998, 1702005, 1702012, 1911996, 1962006, 1962011, 2031991, 2141996, 2182013, 2221999, 2312007, 2331996, 2332011, 2461981, 2461996, 2462005, 2502006, 2681996, 2682009, 2752013, 2761997, 2762006, 2762013, 2882007, 2882012, 3202004, 3442005, 3481982, 3481998, 3482009, 3561990, 3561995, 3562001, 3562006, 3602001, 3602006, 3642000, 3642007, 3682004, 3682006, 3682012, 3762001, 3802005, 3921981, 3921990, 3922000, 3922005, 3922010, 3982011, 4002001, 4002007, 4002014, 4101982, 4101990, 4101996, 4102001, 4102005, 4102010, 4142014, 4172003, 4172011, 4222013, 4281996, 4342014, 4401997, 4582006, 4582012, 4662007, 4841981, 4841990, 4841995, 4841996, 4842000, 4842005, 4842012, 4981996, 4982002, 4982006, 5042001, 5042007, 5042011, 5282006, 5282012, 5541998, 5542004, 5542011, 5661990, 5661995, 5662000, 5662011, 5781996, 5782007, 5861997, 5862001, 5862012, 6041996, 6042001, 6042006, 6042012, 6081996, 6082001, 6082012, 6161989, 6161997, 6162005, 6162012, 6301995, 6302001, 6342010, 6421998, 6422005, 6422012, 6431990, 6431995, 6432006, 6432011, 6462007, 6462012, 6822003, 7022002, 7022012, 7031990, 7031998, 7042001, 7042006, 7051995, 7052005, 7052011, 7101982, 7101990, 7101996, 7102001, 7102006, 7162001, 7162012, 7241990, 7241995, 7242000, 7242007, 7242011, 7521981, 7521996, 7521999, 7522006, 7522011, 7561989, 7561996, 7562007, 7642007, 7802006, 7802011, 7882013, 7921990, 7921996, 7922001, 7922007, 7922011, 8002001, 8041996, 8042006, 8042011, 8071998, 8072001, 8182001, 8182008, 8182013, 8261998, 8262005, 8342001, 8401981, 8401995, 8401999, 8402006, 8402011, 8542007, 8581996, 8582006, 8582011, 8602011, 8621996, 8622000, 8872014, 8912005, 8942007, 9111996, 9112001, 9121996, 9122001, 9141998), c_dcpo = c("Albania", "Albania", "Algeria", "Algeria", "Andorra", "Azerbaijan", "Azerbaijan", "Argentina", "Argentina", "Argentina", "Argentina", "Argentina", "Australia", "Australia", "Australia", "Australia", "Bangladesh", "Bangladesh", "Armenia", "Armenia", "Bosnia and Herzegovina", "Brazil", "Brazil", "Bulgaria", "Bulgaria", "Belarus", "Belarus", "Belarus", "Canada", "Canada", "Chile", "Chile", "Chile", "Chile", "Chile", "China", "China", "China", "China", "China", "Taiwan", "Taiwan", "Colombia", "Colombia", "Colombia", "Colombia", "Croatia", "Cyprus", "Cyprus", "Czech Republic", "Dominican Republic", "Ecuador", "El Salvador", "Ethiopia", "Estonia", "Estonia", "Finland", "Finland", "Finland", "France", "Georgia", "Georgia", "Palestinian Territory", "Germany", "Germany", "Germany", "Ghana", "Ghana", "Guatemala", "Hong Kong", "Hungary", "Hungary", "Hungary", "India", "India", "India", "India", "Indonesia", "Indonesia", "Iran", "Iran", "Iraq", "Iraq", "Iraq", "Israel", "Italy", "Japan", "Japan", "Japan", "Japan", "Japan", "Kazakhstan", "Jordan", "Jordan", "Jordan", "Korea", "Korea", "Korea", "Korea", "Korea", "Korea", "Kuwait", "Kyrgyzstan", "Kyrgyzstan", "Lebanon", "Latvia", "Libya", "Lithuania", "Malaysia", "Malaysia", "Mali", "Mexico", "Mexico", "Mexico", "Mexico", "Mexico", "Mexico", "Mexico", "Moldova", "Moldova", "Moldova", "Morocco", "Morocco", "Morocco", "Netherlands", "Netherlands", "New Zealand", "New Zealand", "New Zealand", "Nigeria", "Nigeria", "Nigeria", "Nigeria", "Norway", "Norway", "Pakistan", "Pakistan", "Pakistan", "Peru", "Peru", "Peru", "Peru", "Philippines", "Philippines", "Philippines", "Poland", "Poland", "Poland", "Poland", "Puerto Rico", "Puerto Rico", "Qatar", "Romania", "Romania", "Romania", "Russia", "Russia", "Russia", "Russia", "Rwanda", "Rwanda", "Saudi Arabia", "Singapore", "Singapore", "Slovakia", "Slovakia", "Vietnam", "Vietnam", "Slovenia", "Slovenia", "Slovenia", "South Africa", "South Africa", "South Africa", "South Africa", "South Africa", "Zimbabwe", "Zimbabwe", "Spain", "Spain", "Spain", "Spain", "Spain", "Sweden", "Sweden", "Sweden", "Sweden", "Sweden", "Switzerland", "Switzerland", "Switzerland", "Thailand", "Trinidad and Tobago", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkey", "Turkey", "Turkey", "Turkey", "Uganda", "Ukraine", "Ukraine", "Ukraine", "Macedonia", "Macedonia", "Egypt", "Egypt", "Egypt", "United Kingdom", "United Kingdom", "Tanzania", "United States", "United States", "United States", "United States", "United States", "Burkina Faso", "Uruguay", "Uruguay", "Uruguay", "Uzbekistan", "Venezuela", "Venezuela", "Yemen", "Serbia", "Zambia", "Serbia", "Serbia", "Montenegro", "Montenegro", "Bosnia and Herzegovina" ), y_dcpo = c(1998, 2002, 2002, 2013, 2005, 1997, 2011, 1984, 1991, 1995, 1999, 2006, 1981, 1995, 2005, 2012, 1996, 2002, 1997, 2011, 2001, 1991, 2006, 1997, 2005, 1990, 1996, 2011, 2000, 2006, 1990, 1996, 2000, 2006, 2011, 1990, 1995, 2001, 2007, 2012, 2006, 2012, 1997, 1998, 2005, 2012, 1996, 2006, 2011, 1991, 1996, 2013, 1999, 2007, 1996, 2011, 1981, 1996, 2005, 2006, 1996, 2009, 2013, 1997, 2006, 2013, 2007, 2012, 2004, 2005, 1982, 1998, 2009, 1990, 1995, 2001, 2006, 2001, 2006, 2000, 2007, 2004, 2006, 2012, 2001, 2005, 1981, 1990, 2000, 2005, 2010, 2011, 2001, 2007, 2014, 1982, 1990, 1996, 2001, 2005, 2010, 2014, 2003, 2011, 2013, 1996, 2014, 1997, 2006, 2012, 2007, 1981, 1990, 1995, 1996, 2000, 2005, 2012, 1996, 2002, 2006, 2001, 2007, 2011, 2006, 2012, 1998, 2004, 2011, 1990, 1995, 2000, 2011, 1996, 2007, 1997, 2001, 2012, 1996, 2001, 2006, 2012, 1996, 2001, 2012, 1989, 1997, 2005, 2012, 1995, 2001, 2010, 1998, 2005, 2012, 1990, 1995, 2006, 2011, 2007, 2012, 2003, 2002, 2012, 1990, 1998, 2001, 2006, 1995, 2005, 2011, 1982, 1990, 1996, 2001, 2006, 2001, 2012, 1990, 1995, 2000, 2007, 2011, 1981, 1996, 1999, 2006, 2011, 1989, 1996, 2007, 2007, 2006, 2011, 2013, 1990, 1996, 2001, 2007, 2011, 2001, 1996, 2006, 2011, 1998, 2001, 2001, 2008, 2013, 1998, 2005, 2001, 1981, 1995, 1999, 2006, 2011, 2007, 1996, 2006, 2011, 2011, 1996, 2000, 2014, 2005, 2007, 1996, 2001, 1996, 2001, 1998), stringsAsFactors = F)
 
 #Latinobarometro
 lb.years <- c(1998, 2002, 2004, 2008, 2009)
@@ -170,20 +185,27 @@ for (i in seq(length(lb.years))) {
 }
 
 # Americas Barometer (This doesn't really work; LAPOP stata file has issues)
-amb <- read.csv(paste0(datapath, "LAPOP/2004-2012/AmericasBarometer Merged 2004-2012 Rev1.5_FREE.csv"), as.is=T)
-amb$country <- countrycode(amb$pais, "country.name", "country.name", warn=T)
-amb$country[grep("Canad", amb$pais, useBytes=T)] <- "CANADA"
-amb$country[grep("Hait", amb$pais, useBytes=T)] <- "HAITI"
-amb$country[grep("xico", amb$pais, useBytes=T)] <- "MEXICO"
-amb$country[grep("Panam", amb$pais, useBytes=T)] <- "PANAMA"
-amb$country[grep("Per", amb$pais, useBytes=T)] <- "PERU"
-amb$country[amb$pais=="Belice"] <- "BELIZE"
-amb$country[amb$pais=="Brasil"] <- "BRAZIL"
-amb$country[amb$pais=="Estados Unidos"] <- "UNITED STATES"
+amb <- read_dta(paste0(datapath, "LAPOP/2004-2014/AmericasBarometer Grand Merge 2004-2014 v3.0_FREE.dta"))
+amb0 <- amb
+
+co <- gsub("\xed", "i", names(attr(amb$pais, "labels")))
+amb$pais0 <- as.integer(amb$pais)
+amb <- left_join(amb, data.frame(pais = c(1:17, 21:29, 40:41),
+                   country = co, stringsAsFactors = F))
+amb$pais <- amb$pais0
+
+cy_data <- amb %>% group_by(pais, year) %>%
+  summarize(c_dcpo = first(country), y_dcpo = as.numeric(round(mean(year)))) %>%
+  ungroup()
+dump("cy_data")
+temp <- readLines("dumpdata.R")
+temp <- paste(temp, sep=" ", collapse=" ")
+temp <- gsub("cy_data <- structure\\(list", "data.frame", temp)
+temp <- gsub("\\), class = .*", ", stringsAsFactors = F)", temp)
 
 surv <- "amb.combo"
-cy <- get.cy.no.id(amb, orig_cvar = "pais", orig_yvar = "year")
-write(cy, file = paste0("cy_data/", surv, ".txt"))
+write(temp, file = paste0("data-raw/cy_data/", surv, ".txt"))
+
 
 # ESS
 ess.combo <- read.dta(paste0(datapath, "ESS/esscumulative1_5/ess1-5_cumulative_e01_1.dta"), convert.factors=F)
@@ -210,7 +232,14 @@ cy <- get.cy(ess6)
 write(cy, file = paste0("cy_data/", surv, ".txt"))
 
 ess7 <- read.dta(paste0(datapath, "ESS/ESS7e01.stata/ESS7e01.dta"), convert.factors=F)
+ess7$country <- countrycode(ess7$cntry, "iso2c", "country.name")
+ess7$year <- ess7$inwyye
+ess7$year[ess7$year==9999] <- ess7$inwyys[ess7$year==9999]
 
+id <- "cntry"
+surv <- "ess7"
+cy <- get.cy(ess7)
+write(cy, file = paste0("data-raw/cy_data/", surv, ".txt"))
 
 # EVS
 evs.combo <- read.csv(paste0(datapath, "EVS/ZA4804_v2-0-0.dta/ZA4804_v2-0-0.csv"), as.is=T)

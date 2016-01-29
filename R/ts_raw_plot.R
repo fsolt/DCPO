2c022ed5a451cab79fa00b265c0427612a1e00d6
Frederick Solt <fsolt@mac.com>
2016-01-29 11:36:13 -0600

    plot selected countries (rather than most data-rich)

--- R/ts_raw_plot.R
+++ R/ts_raw_plot.R
@@ -1,8 +1,17 @@
-# tolerance trends, estimate plus raw data, eight countries
+# tolerance trends, estimate plus raw data, selected countries
 
-c8_rd <- gm_a %>% filter(ccode <= 8) %>% mutate(estimate = y_r/n) %>%
-  left_join(g_res, by = c("rcode", "variable")) %>% rename(gamma_mean = mean)
-c8_res <- a_res %>% filter(kk <= 8)
+countries <- c("United States", "Ireland", "Sweden", "Russia", "Japan",
+               "Argentina", "Spain", "South Africa", "India", "Australia")
+
+c8_rd <- gm_a %>%
+  filter(country %in% countries) %>%
+  mutate(estimate = y_r/n,
+         country = factor(country, levels = countries)) %>%
+  left_join(g_res, by = c("rcode", "variable")) %>%
+  rename(gamma_mean = mean)
+c8_res <- a_res %>%
+  filter(country %in% countries) %>%
+  mutate(country = factor(country, levels = countries))
 
 p_rd <- ggplot(data = c8_rd, aes(x = year, y = estimate)) +
   geom_line(aes(color = variable, alpha = gamma_mean)) +
@@ -12,8 +21,8 @@ p_rd <- ggplot(data = c8_rd, aes(x = year, y = estimate)) +
   labs(x = NULL, y = "Tolerance") +
   geom_ribbon(data = c8_res, aes(ymin = lb, ymax = ub, linetype=NA), alpha = .25) +
   geom_line(data = c8_res) +
-  facet_wrap(~country, ncol = 4) +
+  facet_wrap(~country, nrow = 2) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         strip.background = element_rect(fill = "white", colour = "white"))
 
-ggsave("paper/figures/8c_line.pdf", plot = p_rd, width = 6, height = 4)
+ggsave("paper/figures/sc_line.pdf", plot = p_rd, width = 6, height = 4)

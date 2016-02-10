713b534b788237e51bd1df2938f50854959f2672
Frederick Solt <fsolt@mac.com>
2016-02-10 15:37:48 -0600

    update for KTQR

--- R/ts_raw_plot.R
+++ R/ts_raw_plot.R
@@ -3,19 +3,19 @@
 countries <- c("United States", "Ireland", "Sweden", "Russia", "Japan",
                "Argentina", "Spain", "South Africa", "India", "Australia")
 
-c8_rd <- gm_a %>%
+c8_rd <- gm %>%
   filter(country %in% countries) %>%
   mutate(estimate = y_r/n,
          country = factor(country, levels = countries)) %>%
-  left_join(g_res, by = c("rcode", "variable")) %>%
+  left_join(g_res, by = c("rcode", "variable_cp")) %>%
   rename(gamma_mean = mean)
 c8_res <- a_res %>%
   filter(country %in% countries) %>%
   mutate(country = factor(country, levels = countries))
 
 p_rd <- ggplot(data = c8_rd, aes(x = year, y = estimate)) +
-  geom_line(aes(color = variable, alpha = gamma_mean)) +
-  geom_point(aes(color = variable, alpha = gamma_mean)) + theme_bw() +
+  geom_line(aes(color = variable_cp, alpha = gamma_mean)) +
+  geom_point(aes(color = variable_cp, alpha = gamma_mean)) + theme_bw() +
   theme(legend.position="none") +
   coord_cartesian(xlim = c(1975, 2016), ylim = c(0, 1)) +
   labs(x = NULL, y = "Tolerance") +

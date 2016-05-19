22a45c2407987ae5225aeec133198f05a2a9ef3e
Frederick Solt <fsolt@mac.com>
2016-05-19 15:13:20 -0500

    add eb_datasets files for Erico

---
+++ data-raw/eb_datasets.R
@@ -0,0 +1,15 @@
+# eb_datasets.csv
+
+library(gesis)
+
+if(!dir.exists("downloads")) dir.create("downloads")
+gesis_remDr <- setup_gesis(download_dir = "downloads")
+
+login_gesis(gesis_remDr)
+download_dataset(gesis_remDr, doi = 0989, filetype = "dta", purpose = 1)
+
+dir("downloads")
+gesis_remDr$Close()
+gesis_remDr$closeServer()
+
+browse_codebook(doi = 0989)

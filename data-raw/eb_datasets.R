c8eacd7f38f5d1b7ac808db42d2df8fa231c8d7d
Frederick Solt <fsolt@mac.com>
2016-11-26 13:17:01 -0600

    clean up

--- data-raw/eb_datasets.R
+++ data-raw/eb_datasets.R
@@ -6,7 +6,7 @@ if(!dir.exists("downloads")) dir.create("downloads")
 gesis_remDr <- setup_gesis(download_dir = "downloads")
 
 login_gesis(gesis_remDr)
-download_dataset(gesis_remDr, doi = 0989, filetype = "dta", purpose = 1)
+download_dataset(gesis_remDr, doi = "0989", filetype = "dta", purpose = 1)
 
 dir("downloads")
 gesis_remDr$Close()

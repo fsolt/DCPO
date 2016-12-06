6899f87abbe18db09b6cbc6910f416b53228ff2c
Frederick Solt <fsolt@mac.com>
2016-12-06 12:38:18 -0600

    update

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,5 +1,4 @@
-library(readr)
-library(dplyr)
+library(tidyverse)
 
 library(foreign)
 library(haven)
@@ -8,16 +7,24 @@ library(beepr)
 library(rstan)
 library(stringr)
 
-library(ggplot2)
 
 datasets_table <- read_csv("data-raw/datasets_table.csv")
 gm0 <- read_csv("data-raw/surveys_gm.csv")
 gm <- dcpo_setup(gm0)
 write_csv(gm, "data/all_data_gm.csv")
 
-gm <- read_csv("data/all_data_gm.csv")
-gm_a <- gm %>% filter(cc_rank>=10 & firstyr!=lastyr) %>%
+gm <- read_csv("data/all_data_gm.csv") %>%
+  group_by(ccode) %>%
+  mutate(tq = length(unique(paste(tcode, qcode))),
+         year_obs = length(unique(tcode))) %>%
+  ungroup()
+gm_2y <- gm %>%
+  filter(year_obs >= 2) %>%
   mutate(ccode = as.numeric(factor(ccode)))
+gm_3y <- gm %>%
+  filter(year_obs >= 3) %>%
+  mutate(ccode = as.numeric(factor(ccode)))
+
 
 red0 <- read_csv("../Redistribution/data-raw/redist_vars.csv")
 red <- dcpo_setup(red0)

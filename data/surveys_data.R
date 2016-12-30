57df55adfffba9c012443881d4b4f3f53fa04e19
Frederick Solt <fsolt@mac.com>
2016-12-29 21:24:49 -0400

    script re-build and re-install

--- data/surveys_data.R
+++ data/surveys_data.R
@@ -1,7 +1,9 @@
 library(readr)
 
+detach("package:DCPO", unload=TRUE)
 surveys_data <- read_csv("data/surveys_data.csv")
 save(surveys_data, file = "data/surveys_data.rda")
-
+f <- devtools::build(); system(paste0("R CMD INSTALL --build ", f))
+library(DCPO)
 
 # devtools::use_data(surveys_data, internal = TRUE, overwrite = TRUE)

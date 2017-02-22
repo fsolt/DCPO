5e3942516d6e9f94c9f3a44acad179ada82360d0
Frederick Solt <fsolt@mac.com>
2017-02-22 10:55:23 -0600

    add pew

--- R/get_data.R
+++ R/get_data.R
@@ -4,7 +4,7 @@ library(stringr)#
 library(rio)    #
 library(purrr)  #
 library(icpsrdata)
-
+library(pewdata)
 
 #' @import rio
 #' @import icpsrdata
@@ -14,7 +14,7 @@ library(icpsrdata)
 #' @importFrom haven read_por
 
 s <- login()
-ds <- read_csv("data/new_surveys_data.csv")[17:21,]
+ds <- read_csv("data/new_surveys_data.csv")
 
 pwalk(list(ds$archive, ds$surv_program, ds$file_id), function(archive, surv_program, file_id) {
     dl_dir <- file.path("../data",
@@ -30,19 +30,23 @@ pwalk(list(ds$archive, ds$surv_program, ds$file_id), function(archive, surv_prog
       if(data_file %>% str_detect(".zip$")) {
         unzip(file.path(new_dir, data_file), exdir = new_dir)
         unlink(file.path(new_dir, data_file))
-        data_file <- list.files(path = new_dir) %>% str_subset(".dta") %>% last()
+        data_file <- list.files(path = new_dir) %>%
+          str_subset(".dta") %>%
+          last()
       }
       convert(file.path(new_dir, data_file),
               str_replace(file.path(new_dir, data_file), ".dta", ".RData"))
     } # end gesis
     if (archive=="icpsr") {
-      file_id <- as.numeric(file_id)
+      file_id <- file_id %>% str_replace("ICPSR_", "") %>% as.numeric(file_id)
       icpsr_download(file_id, download_dir = dl_dir)
       new_dir <- file.path(dl_dir, paste0("ICPSR_", file_id %>% sprintf("%05d", .)))
       new_dir2 <- file.path(dl_dir, paste0("ICPSR_", file_id %>% sprintf("%05d", .)), "DS0001")
       data_file <- list.files(path = new_dir2) %>% str_subset(".dta") %>% last()
       if (is.na(data_file)) {
-        data_file <- list.files(path = new_dir2) %>% str_subset(".por") %>% last()
+        data_file <- list.files(path = new_dir2) %>%
+          str_subset(".por") %>%
+          last()
       }
       if (str_detect(data_file, ".por")) {
         # workaround for rio bug importing .por
@@ -53,6 +57,20 @@ pwalk(list(ds$archive, ds$surv_program, ds$file_id), function(archive, surv_prog
                 str_replace(file.path(new_dir, data_file), ".dta", ".RData"))
       }
     } # end icpsr
+    if (archive=="pew") {
+      dir.create(dl_dir, recursive = TRUE, showWarnings = FALSE)
+      old_files <- list.files(dl_dir)
+      pew_download(surv_program, file_id, download_dir = dl_dir, delete_zip = FALSE)
+      new_dir <- list.files(dl_dir, ".zip")[!list.files(dl_dir, ".zip") %in% old_files] %>%
+        str_replace(".zip", "")
+      unlink(file.path(dl_dir, list.files(dl_dir, ".zip")[!list.files(dl_dir, ".zip") %in% old_files]))
+      t <- file.rename(file.path(dl_dir, new_dir), file.path(dl_dir, file_id))
+      data_file <- list.files(path = file.path(dl_dir, file_id)) %>%
+        str_subset(".sav") %>%
+        last()
+      convert(file.path(dl_dir, file_id, data_file),
+              file.path(dl_dir, file_id, paste0(file_id, ".RData")))
+    } #end pew
 })
 
 

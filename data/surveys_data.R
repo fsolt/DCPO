63043e73686e36e4f46a3139c7d71a7149c64924
Frederick Solt <fsolt@mac.com>
2017-03-09 13:27:11 -0600

    switch surveys_data to new format

--- data/surveys_data.R
+++ data/surveys_data.R
@@ -1,7 +1,7 @@
 library(readr)
 
 detach("package:DCPO", unload=TRUE)
-surveys_data <- read_csv("data/surveys_data.csv")
+surveys_data <- read_csv("data/new_surveys_data.csv")
 #save(surveys_data, file = "data/surveys_data.rda")
 devtools::use_data(surveys_data, overwrite = TRUE)
 rm(surveys_data)

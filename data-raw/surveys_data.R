5726e9e9a066cfaf3db0be802c624312051e75a6
Frederick Solt <frederick-solt@uiowa.edu>
2018-12-14 23:07:17 +0100

    switch to usethis::use_data

--- data-raw/surveys_data.R
+++ data-raw/surveys_data.R
@@ -5,7 +5,7 @@ detach("package:DCPO", unload=TRUE)
 surveys_data <- read_csv("data/surveys_data.csv",
                          col_type = cols(year = "i", .default = "c"))
 save(surveys_data, file = "data/surveys_data.rda")
-devtools::use_data(surveys_data, overwrite = TRUE)
+usethis::use_data(surveys_data, overwrite = TRUE)
 rm(surveys_data)
 
 devtools::install('.', local=FALSE)

417634c598035f6aa411bc119679de5c538ef092
Frederick Solt <frederick-solt@uiowa.edu>
2019-06-15 15:37:58 -0500

    clean up

--- R/get_surveys.R
+++ R/get_surveys.R
@@ -241,7 +241,7 @@ get_surveys <- function(vars,
         haven::read_dta(file.path(new_dir, data_file), encoding = "latin1") %>%
           rio::export(file.path(new_dir, paste0(file_id, ".RData")))
       } else {
-        zip_file <- list.files(path = new_dir) %>% str_subset("STATA.zip")
+        zip_file <- list.files(path = new_dir) %>% str_subset("(STATA|stata).*zip$")
         utils::unzip(file.path(new_dir, zip_file), exdir = new_dir)
         data_file <- list.files(path = new_dir) %>% str_subset("dta") %>% last()
         haven::read_dta(file.path(new_dir, data_file), encoding = "latin1") %>%

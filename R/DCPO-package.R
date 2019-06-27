d6900f3211efab0d5ec43c7df60827d3e3c2a8ba
Frederick Solt <frederick-solt@uiowa.edu>
2019-06-27 16:16:29 -0500

    update

--- R/DCPO-package.R
+++ R/DCPO-package.R
@@ -1,12 +1,17 @@
-#' Dynamic Comparative Public Opinion
+#' DCPO: Dynamic Comparative Public Opinion
+#'
+#' @description DCPO estimates dynamic comparative public opinion
 #'
 #' @docType package
 #' @name DCPO-package
 #' @aliases DCPO
 #' @useDynLib DCPO, .registration = TRUE
-#'
-#' @import Rcpp
 #' @import methods
-#' @import bayesplot
+#' @import Rcpp
 #' @import rstantools
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#'
 NULL
